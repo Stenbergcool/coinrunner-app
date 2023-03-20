537f5f6e0a0cd999141dbf525cb93d5eff453231
Alexander <stenberg.alexander@hotmail.com>
2023-03-20 16:07:31 +0100

    map added

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container_map}>
+        <MapView style={styles.map} followsUserLocation={true} showsUserLocation={true} showsCompass={true} showsBuildings={true}/>
+      </View>
+      <View style={styles.underBar}>
+          <Text>UnderBar</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +22,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container_map: {
+    width:'100%',
+    height:'100%'
+  },
+  map: {
+    flex: 1,
+    height: "100%",
+  },
+  underBar: {
+    height: 180,
+    width: "100%",
+    backgroundColor: "white",
+    borderTopWidth: 4,
+    borderTopColor: "black"
   },
 });
