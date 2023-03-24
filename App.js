4dd3dbe231108aee6258b4447d9a9f66d413cc7f
Alexander <stenberg.alexander@hotmail.com>
2023-03-24 18:48:36 +0100

    user, setting icon n little more

--- App.js
+++ App.js
@@ -6,11 +6,14 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import coinHandler from './models/coinHandler';
 
 const coinIcon =  require('./assets/290-coin-flat.gif');
+const userIcon =  require('./assets/user.png');
+const settingIcon =  require('./assets/settings.png');
 
 
 export default function App() {
   const [coins, setCoins] = useState([])
   const [markers, setMarkers] = useState(null);
+  const [followUser, setFollowUser] = useState(true);
   const count = useRef(0);
 
   useEffect(() => {
@@ -35,23 +38,36 @@ export default function App() {
     setMarkers(marks)
   }, [coins]);
 
+  function followOrNot() {
+    if(followUser) {
+      setFollowUser(false)
+    } else {
+      setFollowUser(true)
+    }
+    console.log(followUser)
+  }
 
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container_map}>
-        <MapView style={styles.map} followsUserLocation={true} showsUserLocation={true} showsCompass={true} showsBuildings={true}>
+        <MapView style={styles.map} followsUserLocation={followUser} showsMyLocationButton={true} showsUserLocation={true} showsCompass={true} showsBuildings={true}>
           {markers}
         </MapView>
 
       </View>
       <View style={styles.underBar}>
         <View style={styles.mapTrackerClick}>
-          <Text>
+          <Text onPress={(e) => {followOrNot()}}>
             Click
           </Text>
         </View>
-          <Text>UnderBar</Text>
+        <View style={styles.profileView}>
+          <Image source={userIcon}/>
+        </View>
+        <View style={styles.settingView}>
+          <Image source={settingIcon}/>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -78,10 +94,22 @@ const styles = StyleSheet.create({
     right: 10,
   },
   underBar: {
-    height: 180,
+    position: 'absolute',
+    bottom: 0,
+    height: 100,
     width: "100%",
-    backgroundColor: "white",
-    borderTopWidth: 4,
-    borderTopColor: "black"
+    backgroundColor: "#6B7A8F",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderTopWidth: 10,
+    borderTopColor: "#6B7A8F",
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  settingView: {
+    marginLeft: 'auto'
+  },
+  profileView: {
+
   },
 });
